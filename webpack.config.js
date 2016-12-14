06ac1917c4406c6af0ec30f684cbb53d14cefa71
genghaode <genghaode@126.com>
2016-12-14 12:30:50 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path')
+var webpack = require('webpack')
+var htmlWebpackPlugin = require('html-webpack-plugin')
+
+
+var config = {
+	entry: path.resolve(__dirname, './src/index.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: 'dist',
+		inline: true,
+		port: 8080,
+		stats: {
+			colors: true
+		}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/
+			},{
+				test: /\.css$/,
+				loader: 'style!css',
+				include: path.resolve(__dirname, 'src')
+			},{
+				test: '\.less$',
+				loader: 'style!css!less',
+				include: path.resolve(__dirname, 'src')
+			}
+		]
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			title: 'test',
+			template: './src/index.html'
+		})
+	]
+}
+
+module.exports = config
